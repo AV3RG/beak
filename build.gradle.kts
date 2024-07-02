7970bf58cbd0c74a2a1de73f5815c7fb932ad920
RG <goyalrohan2020@gmail.com>
2024-07-02 20:02:45 +0530

    fix: publish ready

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,11 @@ plugins {
     kotlin("jvm") version "1.9.23"
     id("java-gradle-plugin")
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "gg.rohan"
-version = "1.0-SNAPSHOT"
+version = "0.1"
 
 repositories {
     mavenCentral()
@@ -28,17 +29,48 @@ fun relocateDep(from: String, to: String = "${relocatePrefix}.${from.split(".").
     return tasks.shadowJar.get().relocate(from, to)
 }
 
+tasks.register("loadPropertiesFromFile") {
+    doLast {
+        val file = File(project.rootDir, ".env")
+        if (!file.exists()) {
+            throw IllegalStateException("local.properties does not exist")
+        }
+        file.readLines().forEach {
+            val (key, value) = it.split("=")
+            System.setProperty(key, value)
+        }
+    }
+}
+
+tasks.register("loadPropertiesFromEnv") {
+    doLast {
+        sequenceOf(
+            Pair("GRADLE_PUBLISH_KEY", "gradle.publish.key"),
+            Pair("GRADLE_PUBLISH_SECRET", "gradle.publish.secret")
+        ).forEach {
+            System.setProperty(it.second, System.getenv(it.first)!!)
+        }
+    }
+}
+
 tasks {
     shadowJar {
         archiveBaseName.set("beak")
         archiveClassifier.set("")
         archiveVersion.set("")
 
-        val inclusions = sequenceOf("gg/rohan/beak", "com/github/mwiede/jsch", "com/squareup/retrofit2/retrofit", "com/squareup/okhttp3/okhttp", "com/squareup/okio/okio","org/jetbrains/kotlinx/kotlinx-coroutines-core")
+        val inclusions = sequenceOf(
+            "gg/rohan/beak",
+            "com/github/mwiede/jsch",
+            "com/squareup/retrofit2/retrofit",
+            "com/squareup/okhttp3/okhttp",
+            "com/squareup/okio/okio",
+            "org/jetbrains/kotlinx/kotlinx-coroutines-core"
+        )
         .map { it.split('/' ) }
-        .map {
+        .map { sections ->
             var inc = 0
-            it.fold(mutableListOf<String>()) { acc, it ->
+            sections.fold(mutableListOf<String>()) { acc, it ->
                 for (a in acc.filter { it.split('/').size == inc }) {
                     acc += "$a/$it"
                 }
@@ -48,7 +80,8 @@ tasks {
             }
 
         }
-        .flatten().toList()
+        .flatten().toMutableList()
+        inclusions.add("META-INF")
 
         include { dep ->
             inclusions.any { dep.path.startsWith(it) }
@@ -66,4 +99,38 @@ tasks {
         dependsOn(shadowJar)
     }
 
+    publishPlugins {
+        dependsOn(
+            when(System.getenv("PROPS_LOADING")?.lowercase()) {
+                "env" -> {"loadPropertiesFromEnv"}
+                else -> {"loadPropertiesFromFile"}
+            }
+        )
+    }
+
+}
+
+gradlePlugin {
+    website.set("https://github.com/AV3RG/beak")
+    vcsUrl.set("https://github.com/AV3RG/beak")
+    plugins {
+        create("beak") {
+            id = "gg.rohan.beak"
+            implementationClass = "gg.rohan.beak.BeakPlugin"
+            displayName = "Beak"
+            description = "Gradle plugin to help with deployment to remote servers on pterodactyl"
+            tags.set(listOf(
+                "Pterodactyl",
+                "Deployment",
+                "Remote",
+                "Server",
+                "MineCraft",
+                "Spigot",
+                "Paper",
+                "BungeeCord",
+                "Waterfall",
+                "Velocity"
+            ))
+        }
+    }
 }
